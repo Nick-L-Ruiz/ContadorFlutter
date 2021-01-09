0ec7d837e3726fa040b9dcbddccae8eb52c7dbf7
unknown <nicklom23@gmail.com>
2021-01-09 09:32:22 -0600

    proyecto de practica 1 de flutter, uso de statelesswidget, statefulwidget y buenas practicas como la separacion de la logica en varios archivos para una lectura comoda

---
+++ android/app/src/main/kotlin/com/example/poyecto1_contador/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poyecto1_contador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:poyecto1_contador/src/app.dart';
+
+void main(){
+  runApp(MyApp());
+}
